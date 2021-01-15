9990d1c01607f10ada721b408832a17aff7f8627
limthelimn <nuttawan.lim@gmail.com>
2021-01-15 15:27:58 +0700

    lab11/1.2

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
 using namespace std;
 
-int fibonacci(int);
+long long int fibonacci(int);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
+
+    cout << fibonacci(50);
+
     return 0; 
+
 }
 
-int fibonacci(int x){
+long long int fibonacci(int x){
     if(x <= 1) return x;
     else{
         return fibonacci(x - 1) + fibonacci(x - 2);
