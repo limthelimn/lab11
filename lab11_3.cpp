a15998461f428526896cfccb01bc317039455053
limthelimn <nuttawan.lim@gmail.com>
2021-01-15 17:14:26 +0700

    lab11/3

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,42 +1,24 @@
 #include<iostream>
-#include<string>
+
 using namespace std;
 
-template <typename swap>
-void mySwap(swap &,swap &);
+void mySwap(int &,int &); 
 
 int main(){
-	int x, y;
-	string a, b;
-	char p, q;
-	
-	cin >> x >> y >> a >> b >> p >> q;
-	
+	int x,y;
+	cin >> x >> y;
 	cout << "Before swapping:\n";
 	cout << "x = " << x << ", y = " << y << "\n";
-	mySwap(x,y);
+	mySwap (x,y);
 	cout << "After swapping:\n";
 	cout << "x = " << x << ", y = " << y << "\n";
 	
-	cout << "Before swapping:\n";
-	cout << "a = " << a << ", b = " << b << "\n";
-	mySwap(a,b);
-	cout << "After swapping:\n";
-	cout << "a = " << a << ", b = " << b << "\n";
-	
-	cout << "Before swapping:\n";
-	cout << "p = " << p << ", q = " << q << "\n";
-	mySwap(p,q);
-	cout << "After swapping:\n";
-	cout << "p = " << p << ", q = " << q << "\n";
-
 	return 0;
 }
 
-template <typename swap>
-void mySwap(swap &x,swap &y){
-	swap i;
-	i = x;
+void mySwap(int &x,int &y){
+	int i = 0;
+	x = i;
 	x = y;
-	y = i;
+	y = x;
 }
